1c12266e0147fc9045cafe5a1e096ce3426ac524
Tuvshinbayr <Sotgonbaatar93@gmail.com>
2026-06-24 17:59:39 +0800

    init

---
+++ src/test/java/mn/internhub/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package mn.internhub.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
