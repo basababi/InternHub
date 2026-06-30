cefd7f7de9893c5018fe75f6e597fcf3e2197939
Tuvshinbayr <Sotgonbaatar93@gmail.com>
2026-06-30 17:35:10 +0800

    api хандалтын аюулгүй байдал дээр нийтэд харагдах бүх байгууллагын мэдээллийн api-ийг token шалгалгүй get хүсэлтийг хийдэг болгов

--- src/main/java/mn/internhub/demo/config/SecurityConfig.java
+++ src/main/java/mn/internhub/demo/config/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/organization").permitAll()
                         .anyRequest().authenticated())
 //                        .anyRequest().permitAll())
                 .sessionManagement(session -> session
