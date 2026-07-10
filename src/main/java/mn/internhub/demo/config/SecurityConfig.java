6f0cdedf4829ccf419f4a09d3692a91e3e97dfe8
Tuvshinbayr <Sotgonbaatar93@gmail.com>
2026-07-10 14:51:21 +0800

    алдаа засав

--- src/main/java/mn/internhub/demo/config/SecurityConfig.java
+++ src/main/java/mn/internhub/demo/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/organization").permitAll()
